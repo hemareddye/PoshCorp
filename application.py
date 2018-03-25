c3ce0d9dd7c873a5b7c12d565c219cc460a91356
Sandie PS <sandieps@Sandies-MacBook-Air.local>
2018-03-25 21:36:17 +0530

    apis

--- application.py
+++ application.py
@@ -158,13 +158,22 @@ def get():
 
     if GetPrice()['CurrentPrice'][0]['variantsku'] == variantsku:
         if GetPrice()['CurrentPrice'][0]['mrp'] == mrp and GetPrice()['CurrentPrice'][0]['webprice'] == webprice :
+            import pymysql
+            db = pymysql.connect(host="poshlette.cnauabwc9dbm.us-east-1.rds.amazonaws.com", user="sandieps", passwd="Sandie0713", database = "PoshCorp")
+            cursr = db.cursor()
+            cursr.execute("""insert into runs(taskid) values (%s)""", Taskid)
+            db.commit()
             return jsonify( "Run Message: DCN is active" , "Task id generated: " +  str(PricelistAPI()),"Task status:" +  str(PricelistTaskStatus()),"location ref code:"  +   str(LocationInfo()),"Posted for SKU: "  +  str(sku),"Posted for variant sku: "  +   str(variantsku), "Posted MRP:"  +   str(mrp), "Posted Web price:"  +   str(webprice), "Got MRP: "+   str(GetPrice()['CurrentPrice'][0]['mrp']), "Got Webprice: "  +   str(GetPrice()['CurrentPrice'][0]['webprice']))
         elif GetPrice()['CurrentPrice'][0]['mrp'] != mrp and GetPrice()['CurrentPrice'][0]['webprice'] != webprice :
+            db = pymysql.connect(host="poshlette.cnauabwc9dbm.us-east-1.rds.amazonaws.com", user="sandieps", passwd="Sandie0713", database = "PoshCorp")
+            cursr = db.cursor()
+            cursr.execute("""insert into runs(taskid) values (%s)""", Taskid)
+            db.commit()
             return jsonify("Run Message: DCN is not active" , "Task id generated: " +  str(PricelistAPI()),"Task status:" + str(PricelistTaskStatus()),"location ref code:"  +   str(LocationInfo()),"Posted for SKU: "  +  str(sku),"Posted for variant sku: "  +  str(variantsku), "Posted MRP:"  + str(mrp), "Posted Web price:"  +   str(webprice), "Got MRP: "+   str(GetPrice()['CurrentPrice'][0]['mrp']), "Got Webprice: "  +   str(GetPrice()['CurrentPrice'][0]['webprice']))
 
 @application.route('/Pricelistretry', methods=['GET'])
 def Pricelistretry():
-    return get().sku
+    return "wait"
     
 
 if __name__ == '__main__':
